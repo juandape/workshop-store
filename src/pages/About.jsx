98674bc12fbee283f724011d54215fca79ce36c0
Juan David Peña <juandavidp76@hotmail.com>
2023-05-13 09:17:19 -0500

    css about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,34 +1,41 @@
-import './about.css'
+import "./about.css";
 
 function AboutPage() {
   return (
-    <div className="about-container">
+    <>
       <h1>About me</h1>
-      <img src="https://github.com/juandape/workshop-store/blob/main/src/assets/IMG_3661.png" alt="author image" />
-      <h2>Juan David Peña</h2>
-      <p>Fullstack Developer, profesional Bartender, Make It Real student</p>
-      <h2>What I learn</h2>
-      <ul>
-        <li>Create and manage remote and local repos with Git</li>
-        <li>Use of the React library for the creation of web pages</li>
-        <li>Best way to present and deploy the different projects</li>
-      </ul>
-      <h3>
-        github: <a href="https://github.com/juandape">@juandape</a>
-      </h3>
-      <h3>
-        email:
-        <a href="mailto:juandavidp76@gmail.com">juandavidp76@gmail.com</a>
-      </h3>
+    <div className="about-container">
+      <div className="about-container__info">
+      <img
+        src="src\assets\IMG_3661.png"
+        alt="author image"
+        className="about-container--img"
+      />
+      <div className="about-container--description">
+      <h2 className="about-container--description--name">Juan David Peña</h2>
+      <p className="about-container--description--text">Fullstack Developer, profesional Bartender, Make It Real student</p>
+      </div>
+      </div>
+      <div className="about-container__skills">
+        <h2 className="about-container__skills--title">What I learn</h2>
+        <ul className="about-container__skills--list">
+          <li>Create and manage remote and local repos with Git</li>
+          <li>Use of the React library for the creation of web pages</li>
+          <li>Best way to present and deploy the different projects</li>
+        </ul>
+      </div>
+      <div className="about-container__social">
+        <h3>
+          github: <a href="https://github.com/juandape">  @juandape</a>
+        </h3>
+        <h3>
+          email: 
+          <a href="mailto:juandavidp76@gmail.com">  juandavidp76@gmail.com</a>
+        </h3>
+      </div>
     </div>
+    </>
   );
 }
 
 export default AboutPage;
-
-// Nombre
-// Descripción
-// Imagen
-// Lista de 3 cosas que hayas aprendido en este programa
-// Correo
-// Link a tu github

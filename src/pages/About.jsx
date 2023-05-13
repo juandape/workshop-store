875236eabce2de55a0c395a6460c19932359c844
Juan David Peña <juandavidp76@hotmail.com>
2023-05-13 12:37:11 -0500

    img update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ function AboutPage() {
     <div className="about-container">
       <div className="about-container__info">
       <img
-        src="https://github.com/juandape/workshop-store/blob/ce3acac1d870764e93d8cb13027e8347edb40382/src/assets/IMG_3661.png"
+        src='..\src\assets\IMG_3661.png'
         alt="author image"
         className="about-container--img"
       />
